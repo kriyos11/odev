ede57c3510e4b430aae34a177df0cbff596364c5
kriyos11 <bahadirodemis@gmail.com>
2024-11-22 20:04:14 +0300

    ödev dikdortgen_alan.java

---
+++ dikdortgen_alan.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int genislik;
+        int yukseklik;
+        int sonuc;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Genişlik girin:");
+        genislik = scanner.nextInt();
+
+        System.out.println("Yükseklik girin:");
+        yukseklik = scanner.nextInt();
+
+        sonuc = genislik * yukseklik;
+
+        if(genislik == yukseklik || yukseklik == genislik) {
+
+            System.out.println("Girmiş olduğunuz sayılar karenin alanını hesaplar:" +sonuc);
+        }
+        else {
+            System.out.println(sonuc);
+        }
+    }
+}
